9f43dd17c5b7095d0411a4fc5c9f2fe1fe6849a7
VadimVolin <vadim.volin1@gmail.com>
2020-04-17 01:21:41 +0300

    FINISH: basic upload file from form to folder on server

--- src/main/java/vadim/volin/controllers/UpdateUserController.java
+++ src/main/java/vadim/volin/controllers/UpdateUserController.java
@@ -1,34 +1,74 @@
 package vadim.volin.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import vadim.volin.model.User;
 import vadim.volin.services.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.Arrays;
 
 @Controller
 @SessionAttributes("user")
-@RequestMapping
 public class UpdateUserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping(value = "/update/user", produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseBody
-    public String updateUserInfo(@ModelAttribute User user, Model model) {
-        if (user == null) {
-            return "";
+    @PostMapping("/update/user/img")
+//    @ResponseBody
+    public /*ResponseEntity<String>*/ String handleUploadImage(@RequestBody MultipartFile file, @ModelAttribute User user, Model model) {
+        if (file.isEmpty()) {
+//            return new ResponseEntity("Please, select image for uploading!", HttpStatus.OK);
+            model.addAttribute("message", "Please, choose file!");
+            return "redirect:/home";
         }
-        System.out.println(user);
+        try {
+            System.out.println("START");
+            byte[] bytes = file.getBytes();
+            System.out.println(Arrays.toString(bytes));
+            File newFile = new File("/home/vadim/Документы/Spring/springMVC-courses/src/main/webapp/resources/manluck_data/user_img/"
+                    + user.getUsername().toLowerCase().replaceAll("\\s+", "") + ".png");
+            System.out.println(newFile.getAbsolutePath());
+            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(newFile));
+            outputStream.write(bytes);
+            user.setUser_img(newFile.getAbsolutePath());
+            userService.editUser(user);
+            model.addAttribute("user", user);
+            outputStream.close();
+        } catch (IOException e) {
+//            return new ResponseEntity("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            model.addAttribute("message", "Server error, please, try again!");
+            return "redirect:/home";
+        }
+        return "redirect:/home";
+//        return new ResponseEntity("Successfully upload!", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/update/user", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public ResponseEntity<?> updateUserInfo(@ModelAttribute User user, Model model) {
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
         User update = userService.editUser(user);
-        model.addAttribute("user", user);
-        return "Data update";
+        model.addAttribute("user", update);
+        return new ResponseEntity("Successfully upload!", HttpStatus.OK);
     }
 
     @ResponseBody
