400f531cb0f25ba599acbad2b9dda974e6dc0cae
VadimVolin <vadim.volin1@gmail.com>
2020-05-30 17:24:58 +0300

    FIX: start fix system design

--- src/main/java/vadim/volin/util/ProjectUtils.java
+++ src/main/java/vadim/volin/util/ProjectUtils.java
@@ -22,16 +22,6 @@ public class ProjectUtils {
         }
         JsonParser parser = factory.createParser(JSON_TASKS);
         JsonNode rootObject = mapper.readTree(parser);
-//        {
-//          "columns":
-//              {"idCounter":2,"title":"",
-//              "items":[
-//                  {"id":1,"title":"manluck-c1","notesId":[1,2,6,4,5,7,9]},
-//                  {"id":2,"title":"manluck-c2","notesId":[3,8,10]}]}
-//               ,"notes":{
-//                      "idCounter":10,
-//                      "items":[{"id":1,"content":"m1"},{"id":2,"content":"m2"},
-//                      {"id":6,"content":"m6"},{"id":4,"content":"m4"},{"id":5,"content":"m5"},{"id":7,"content":"m7"},{"id":9,"content":"m10"},{"id":3,"content":"m3"},{"id":8,"content":"m8"},{"id":10,"content":"m9"}]}}
 
         ArrayNode columnArray = (ArrayNode) rootObject.get("columns").get("items");
         for (int i = 0; i < columnArray.size(); i++) {
